a3a1c9944d86dd8534d43de3aca65854353b8af4
Stage-sys <78994817+Stage-sys@users.noreply.github.com>
2021-02-25 11:22:40 +0800

    Delete src directory

--- src/src/init.h
+++
@@ -1,6 +0,0 @@
-#ifndef INIT_H
-#define INIT_H
-
-void Init_SR_and_Stack(void);
-
-#endif
--- src/src/string.h
+++
@@ -1,7 +0,0 @@
-#ifndef STRING_H
-#define STRING_H
-
-void __attribute__((noinline)) *memcpy(void *dest, const void *source, unsigned int n);
-void __attribute__((noinline)) *memset(void *dest, int c, unsigned int n);
-
-#endif
