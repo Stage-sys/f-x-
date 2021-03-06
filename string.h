58e0417c5250dfba2e2d5b5d98ef5510d8f611ca
Stage-sys <78994817+Stage-sys@users.noreply.github.com>
2021-03-06 22:20:11 +0800

    Add files via upload

---
+++ string.h
@@ -0,0 +1,8 @@
+#ifndef STRING_H
+#define STRING_H
+
+void *memcpy(void const * dest, void const * src, unsigned int n);
+void *memset(void const * dest, int c, unsigned int n);
+unsigned int strlen(char const string[static 1]);
+
+#endif
