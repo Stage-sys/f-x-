006b6f5d80d7d7321797bef5650ab87994005cc6
Stage-sys <78994817+Stage-sys@users.noreply.github.com>
2021-03-28 07:28:35 +0800

    Delete string.h

--- string.h
+++
@@ -1,8 +0,0 @@
-#ifndef STRING_H
-#define STRING_H
-
-void *memcpy(void const * dest, void const * src, unsigned int n);
-void *memset(void const * dest, int c, unsigned int n);
-unsigned int strlen(char const string[static 1]);
-
-#endif
